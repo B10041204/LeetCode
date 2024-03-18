178ba2558571e560ca1bf6f5dbdafd3b05a423b2
敖梦瑶 <917634995@qq.com>
2024-03-18 20:04:51 +0800

    LeetCode0007提交

---
+++ LeetCode0007/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode0007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().Reverse(123));
+            Console.WriteLine(new Solution().Reverse(-123));
+            Console.WriteLine(new Solution().Reverse(-120));
+            Console.WriteLine(new Solution().Reverse(120));
+            Console.WriteLine(new Solution().Reverse(0));
+            Console.WriteLine(new Solution().Reverse(1534236469));
+            Console.WriteLine(new Solution().Reverse(-2147483648));
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            var flag = x > 0;
+            var queue = new Queue<int>();
+            do
+            {
+                queue.Enqueue(Math.Abs(x % 10));
+                x = x / 10;
+            } while (x != 0);
+
+            if (int.TryParse(string.Join("", queue), out int result))
+            {
+                return flag ? result : 0 - result;
+            }
+            else
+            {
+                return default(int);
+            }
+        }
+    }
+}
