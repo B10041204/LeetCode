034f95a607189efaa58ab6a3e5bf6be3abfe2be1
敖梦瑶 <917634995@qq.com>
2024-03-27 10:19:16 +0800

    二分法求解雨水问题

---
+++ LeetCode0011/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode0011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+            Console.WriteLine(new Solution().MaxArea(new int[] { 1, 1 }));
+            var startTime = DateTimeOffset.Now;
+            Console.WriteLine(new Solution().MaxArea(new Mock().Array));
+            var endTime = DateTimeOffset.Now;
+            Console.WriteLine($"总计耗时：{(endTime - startTime).TotalSeconds}");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            var area = int.MinValue;
+
+            var low = 0;
+
+            var high = height.Length - 1;
+
+            while (low < high)
+            {
+                if (height[low]> height[high])
+                {
+                    var temp = height[high] * (high - low);
+                    if (temp > area)
+                    {
+                        area = temp;
+                    }
+                    high--;
+                }
+                else
+                {
+                    var temp=height[low] * (high - low);
+                    if (temp > area)
+                    {
+                        area = temp;
+                    }
+                    low++;
+                }
+            }
+
+            return area;
+        }
+    }
+}
