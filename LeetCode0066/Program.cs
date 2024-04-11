e98661105d477bf71d4be7ecb1b02ee737b21d33
敖梦瑶 <917634995@qq.com>
2024-04-11 13:46:53 +0800

    LeetCode0066提交

---
+++ LeetCode0066/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode0066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join("", new Solution().PlusOne(new int[] { 0 })));
+            Console.WriteLine(string.Join("", new Solution().PlusOne(new int[] { 1, 2, 3 })));
+            Console.WriteLine(string.Join("", new Solution().PlusOne(new int[] { 9, 9, 9 })));
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var pre = 1;
+            var index = digits.Length - 1;
+            while (index >= 0)
+            {
+                var total = digits[index] + pre;
+                digits[index] = total % 10;
+                pre = total / 10;
+                if (pre == 0) break;
+                index--;
+            }
+            if (index == -1 && pre == 1)
+            {
+                digits = (new int[1] { 1 }).Concat(digits).ToArray();
+            }
+            return digits;
+        }
+    }
+}
