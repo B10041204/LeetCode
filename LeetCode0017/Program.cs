f1de542ec564c93ac79f9e14fbb8cd1f3ccee36f
敖梦瑶 <917634995@qq.com>
2024-04-11 13:17:04 +0800

    LeetCode0058提交

---
+++ LeetCode0017/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode0017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().LetterCombinations("23"));
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+    public class Solution
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            var result=new List<string>();
+            var dict = new Dictionary<char, List<char>>()
+            {
+                { '2', new List<char>() {'a','b','c' } },
+                { '3', new List<char>() {'d','e','f' } },
+                { '4', new List<char>() {'g','h','i' } },
+                { '5', new List<char>() {'j','k','l' } },
+                { '6', new List<char>() {'m','n','o' } },
+                { '7', new List<char>() {'p','q','r','s'} },
+                { '8', new List<char>() {'t','u','v'} },
+                { '9', new List<char>() {'w','x','y','z' } }
+            };
+            for (int i = 0; i < digits.Length; i++)
+            {
+                for (int j = 0; j < dict[digits[i]].Count; j++)
+                {
+
+                }
+            }
+        }
+
+    }
+}
---
+++ LeetCode0058/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode0058
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().LengthOfLastWord("luffy is still joyboy"));
+            Console.WriteLine(new Solution().LengthOfLastWord("Hello World"));
+            Console.WriteLine(new Solution().LengthOfLastWord("   fly me   to   the moon  "));
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var current = s.Length - 1;
+            var pre = int.MinValue;
+            while (current >= 0)
+            {
+                if (s[current] != ' ' && pre == int.MinValue)
+                {
+                    pre = current;
+                }
+                else if ((s[current] == ' ' && pre == int.MinValue) || (s[current] != ' ' && pre != int.MinValue))
+                {
+                    current--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return pre - current;
+        }
+    }
+}
