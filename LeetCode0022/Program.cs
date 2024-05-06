4effc78ad1b1a2b43c64be4c3de57c6bd4a5f8e2
敖梦瑶 <917634995@qq.com>
2024-05-06 18:21:05 +0800

    LeetCode0026提交

---
+++ LeetCode0022/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode0022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Solution().GenerateParenthesis(3);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode0026/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode0026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().RemoveDuplicates(new int[] { 1, 1, 2 }));
+            Console.WriteLine(new Solution().RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var pre = 1;
+            var current = 0;
+            var total = nums.Length;
+            var count = total;
+
+            while (current < total && pre < total)
+            {
+                if (nums[pre] == nums[current])
+                {
+                    pre++;
+                    count--;
+                }
+                else
+                {
+                    nums[++current] = nums[pre++];
+                    
+                }
+            }
+            return count;
+        }
+    }
+}
