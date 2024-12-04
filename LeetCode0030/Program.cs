816ce5aec9d9f79f3c893bedbff9a90e7ab6974a
敖梦瑶 <917634995@qq.com>
2024-12-04 23:13:13 +0800

    LeetCode 0035 submit

---
+++ LeetCode0030/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode0030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LeetCode0035/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode0035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(new Solution().SearchInsert(new int[] { 1, 3, 5, 6 }, 7).ToString());
+        }
+    }
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var pre = 0;
+            var next = nums.Length - 1;
+            var current = (int)Math.Floor((decimal)nums.Length / 2);
+            while(pre<= next)
+            {
+                if(nums[current] == target)
+                {
+                    return current;
+                }
+                else if(nums[current] > target)
+                {
+                    next = current - 1;
+                    current = (pre + next) / 2;
+                }
+                else
+                {
+                    pre = current + 1;
+                    current = (pre + next) / 2;
+                }
+            }
+            if (nums[current] >target)
+            {
+                return current;
+            }
+            else
+            {
+                return current + 1;
+            }
+        }
+    }
+}
