218af3a2389e0bf18ec9facca8f4e18ac68b16ea
敖梦瑶 <917634995@qq.com>
2024-12-06 12:42:42 +0800

    LeetCode 0069 Submit

---
+++ LeetCode0069/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode0069
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(new Solution().MySqrt(2147395600));
+        }
+    }
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            if (x == 0 || x == 1) { return x; }
+
+            for (int i = 1; i < x; i++)
+            {
+                if (i * i > x || i * i < 0) return i - 1;
+            }
+
+            return 1;
+        }
+    }
+}
