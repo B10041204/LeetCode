4ea2aca870e0835e6a78728965895f06a79add97
敖梦瑶 <917634995@qq.com>
2024-12-10 21:08:47 +0800

    LeetCode 205 submit

---
+++ LeetCode0205/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode0205
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Console.WriteLine(new Solution().IsIsomorphic("foo", "bar"));
+            //Console.WriteLine(new Solution().IsIsomorphic("egg", "add"));
+            Console.WriteLine(new Solution().IsIsomorphic("paper", "title"));
+        }
+    }
+    public class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            var dict = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    if (dict[s[i]] != t[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (dict.ContainsValue(t[i]))
+                    {
+                        return false;
+                    }
+                    dict.Add(s[i], t[i]);
+                }
+            }
+            return true;
+        }
+    }
+}
