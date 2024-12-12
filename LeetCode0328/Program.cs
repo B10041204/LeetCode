34b554aef276a7650a224fc46f1d6cdbd38190ba
敖梦瑶 <917634995@qq.com>
2024-12-12 22:12:54 +0800

    1. LeetCode 0015 Submit Timed out;2. Add LeetCode 0328

---
+++ LeetCode0328/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode0328
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var result = new Solution().OddEvenList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
+            while (result != null)
+            {
+                Console.WriteLine(result.val);
+                result = result.next;
+            }
+        }
+    }
+    /**
+    * Definition for singly-linked list.
+    */
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            var current = head;
+            var index = 0;
+            var even = head.next;
+            while (head != null)
+            {
+                if (index % 2 == 0) 
+                {
+                    even.next = head.next.next;
+                    even = even.next;
+                }
+                head = head.next;
+                index++;
+            }
+            return current;
+        }
+    }
+}
